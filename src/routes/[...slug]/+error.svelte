e9e618774df72fa9012f52af4647c8fe64f2ce6c
MKCO <61830362+kosterm14@users.noreply.github.com>
2024-04-25 20:36:50 +0200

    Update +error.svelte

--- src/routes/[...slug]/+error.svelte
+++ src/routes/[...slug]/+error.svelte
@@ -1,22 +1,46 @@
 <script>
-    import { page } from '$app/stores';
+	import { page } from '$app/stores';
+
+	function goBack() {
+		window.history.back();
+	}
 </script>
 
-    <main>
-        <h1>{$page.error.message}</h1>
-        <p>Deze pagina bestaat niet</p>
-    </main> 
+<main>
+	<h1>Oeps, deze pagina bestaat niet</h1>
+	<p>De pagina <em>"{$page.url.pathname}"</em> bestaat niet</p>
+	<button on:click={goBack}>Terug naar de vorige pagina</button>
+</main>
 
 <style>
-    h1,
-    p {
-        font-family: 'Poppins', sans-serif;
-    }
+	h1,
+	p {
+		font-family: 'Poppins', sans-serif;
+	}
 
-    main {
-        margin-top: 5rem;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
+	main {
+		margin-top: 5rem;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	button {
+		margin-top: 1rem;
+		padding: 0.5rem 1rem;
+		font-size: 1rem;
+		color: #fff;
+		background-color: #0064c8;
+		border: none;
+		border-radius: 4px;
+		cursor: pointer;
+	}
+
+	button:hover {
+		background-color: #0051a8;
+	}
+
+    em {
+        color: #0064c8;
     }
 </style>
