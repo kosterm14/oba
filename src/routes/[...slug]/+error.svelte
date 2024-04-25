866e637534b6b8883a4cbfcd80f3feb718048bfe
MKCO <61830362+kosterm14@users.noreply.github.com>
2024-04-25 15:48:33 +0200

    error page v1
    De basiscode om op verder te werken

---
+++ src/routes/[...slug]/+error.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+    <main>
+        <h1>{$page.error.message}</h1>
+        <p>Deze pagina bestaat niet</p>
+    </main>
+
+<style>
+    h1,
+    p {
+        font-family: 'Poppins', sans-serif;
+    }
+
+    main {
+        margin-top: 5rem;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
